64dbb8423caa1b6dc9da00934067f68919bbb40c
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2017-02-02 11:26:16 -0500

    Make tests independent

--- tests.R
+++ tests.R
@@ -1,7 +1,33 @@
 library(testthat)
+source("subsette.R")
 
+
+### Set up ###
 set.seed(4711)
 
+b <- matrix(data = rnorm(25), nrow = 5, ncol = 5)
+dimnames(b) <- list(letters[1:5], letters[1:5])
+
+a <- structure(list(), class = "TestMatrix")
+
+dim.TestMatrix <- function(x) c(5, 5)
+
+dimnames.TestMatrix <- function(x) dimnames(b)
+
+subset_vector <- function(x, i) {
+    # Dispatch to b instead to x
+    b[i, drop = FALSE]
+}
+
+subset_matrix <- function(x, i, j) {
+    # Dispatch to b instead to x
+    b[i, j, drop = FALSE]
+}
+
+`[.TestMatrix` <- subsette(subset_vector = subset_vector, subset_matrix = subset_matrix)
+
+
+### Tests ###
 test_that("subsette constructor works", {
 
     expect_error(subsette())
