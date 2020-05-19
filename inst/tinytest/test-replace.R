2ce78935606546757ba1e52ddd9ed37d98141c6c
Alexander Grueneberg <git@agrueneberg.info>
2020-05-19 16:55:38 -0400

    Split test-crochet.R into test-extract.R and test-replace.R

--- inst/tinytest/test-replace.R
+++ inst/tinytest/test-replace.R
@@ -1,17 +1,3 @@
-# extract
-expect_error(extract())
-expect_error(extract(extract_vector = "derp"))
-expect_error(extract(extract_matrix = "derp"))
-expect_error(extract(extract_vector = "derp", extract_matrix = "derp"))
-expect_error(extract(extract_vector = function() {}, extract_matrix = "derp"))
-expect_error(extract(extract_vector = "derp", extract_matrix = function() {}))
-expect_error(extract(extract_vector = function() {}, extract_matrix = function() {}))
-expect_error(extract(extract_vector = function(x) {}, extract_matrix = function(x) {}))
-expect_error(extract(extract_vector = function(x, i) {}, extract_matrix = function(x, i) {}))
-expect_equal(typeof(extract(extract_vector = function(x, i) {}, extract_matrix = function(x, i, j) {})), "closure")
-
-
-# replace
 expect_error(replace())
 expect_error(replace(replace_vector = "derp"))
 expect_error(replace(replace_matrix = "derp"))
